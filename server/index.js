1ed66200068897b447280ceeafeb2b283979a875
sourabh kumar <sourabhsbg01@gmail.com>
2024-08-05 01:32:56 +0530

    JWT server

--- server/index.js
+++ server/index.js
@@ -6,51 +6,48 @@ const carRouter = require('./routes/carRoute')
 const MongoStore = require('connect-mongo');
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 const key = 'dfgbnsthgse34g';
 
 const PORT = 3001;
 const DB = 'mongodb+srv://sourabh2112:Sk%4012344321@cluster0.itsazuw.mongodb.net/Cars?retryWrites=true&w=majority&appName=Cluster0';
-// const localDB = 'mongodb://localhost:27017/Cars'
+// const DB = 'mongodb://localhost:27017/Cars'
 
-
-const app = express()
+const app = express();
 app.use(bodyParser.json());
-app.use(express.json())
+app.use(cookieParser());
+app.use(express.json());
 app.use(cors({
     origin: 'https://assignment-car-f4hl.vercel.app', // Adjust to your front-end origin
     credentials: true
 }));
 
-
-// Configure session middleware
-app.use(session({
-    secret: key,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        secure: true,
-        maxAge: 1000 * 60 * 60, // Session duration
-        sameSite: 'None',
-        //domain: 'https://assignment-car-f4hl.vercel.app'
-    }
-}));
-
 // Middleware to check if user is authenticated
 const isAuthenticated = (req, res, next) => {
-    if (req.session.user) {
+
+    // console.log(req.cookies);
+    const { token } = req.cookies;
+
+    try {
+        if (!token) {
+            res.status(403).send('please login first');
+        }
+        jwt.verify(token, 'sourabh');
+        // console.log(decode);
         next();
-    } else {
-        res.status(401).json({ message: "Access denied. Please log in." });
+    } catch (error) {
+        console.log(error);
+        res.status(404).send('Invalide token');
     }
 };
 
 app.post("/register", async (req, res) => {
-    const { email, ...otherData } = req.body;
+    const { email, password, role, name } = req.body; // Extract email and password from the body
 
-    // Check if email is provided
-    if (!email) {
-        return res.status(400).json({ message: "Email is required" });
+    // Check if email and password are provided
+    if (!email || !password) {
+        return res.status(400).json({ message: "Email and password are required" });
     }
 
     try {
@@ -61,9 +58,11 @@ app.post("/register", async (req, res) => {
         }
 
         // Create new client
-        const newClient = await ClientModel.create({ email, ...otherData });
+        const newClient = await ClientModel.create({ email, password, role, name });
+
         res.status(201).json(newClient);
     } catch (err) {
+        console.error(err);
         res.status(500).json({ message: "Internal Server Error", error: err.message });
     }
 });
@@ -71,7 +70,8 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
     const { email, password } = req.body;
-    if (!email || !password) {
+    // console.log(email);
+    if (!email && !password) {
         return res.status(400).json({ message: "Enter the Credentials..." });
     }
 
@@ -86,8 +86,33 @@ app.post("/login", async (req, res) => {
             return res.status(401).json({ message: "Password incorrect!!!" });
         }
 
-        req.session.user = user;
-        res.status(200).json({ message: "Success", role: user.role });
+        const token = jwt.sign(
+            {
+                id: user._id,
+                email
+            },
+            'sourabh', //secret
+            {
+                expiresIn: "1h"
+            }
+        );
+        user.token = token;
+
+        const options = {
+            expires: new Date(Date.now() + 1 * 60 * 60 * 1000),
+            httpOnly: true,
+            secure: true,
+            sameSite: 'None'
+        }
+
+        res.status(200).cookie("token", token, options).json({
+            success: true,
+            token,
+            user,
+            role: user.role
+        })
+        // req.session.user = user;
+        // res.status(200).json({ message: "Success", role: user.role });
     } catch (error) {
         res.status(500).json({ message: "Internal Server Error", error });
     }
@@ -95,17 +120,15 @@ app.post("/login", async (req, res) => {
 
 // Logout endpoint
 app.post("/logout", (req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-            return res.status(500).json({ message: "Unable to log out" });
-        } else {
-            res.clearCookie('connect.sid'); // Clear the session cookie
-            res.status(200).json({ message: "Logout successful" });
-        }
+    res.clearCookie('token', {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'None'
     });
+    res.status(200).json({ message: "Logout successful" });
 });
 
-app.use("/api", carRouter);
+app.use("/api", isAuthenticated, carRouter);
 
 mongoose.connect(DB)
     .then(() => {
